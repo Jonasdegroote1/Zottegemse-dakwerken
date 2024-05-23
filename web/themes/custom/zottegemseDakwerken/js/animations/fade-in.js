8f5c212400d8a61df311e4aee66afd5165f9e72d
jonasdegroote1 <degrjona@student.arteveldehs.be>
2024-05-23 12:34:37 +0200

    Add: gsap library + fade-in animatie

---
+++ web/themes/custom/zottegemseDakwerken/js/animations/fade-in.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const elements = document.querySelectorAll('[data-animation="fade-in"]');
+  
+  elements.forEach(element => {
+    const delay = parseFloat(element.dataset.delay) || 0;
+
+    gsap.from(element, {
+      opacity: 0,
+      duration: 1,
+      delay: delay,
+      scrollTrigger: {
+        trigger: element,
+        start: "top 80%",
+        end: "bottom bottom",
+        toggleActions: "play none none none",
+      }
+    });
+  });
+});
