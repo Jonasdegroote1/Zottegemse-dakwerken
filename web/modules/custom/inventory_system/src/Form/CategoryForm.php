722c06d6b9b9a722e98c4a311d5ae9811af5fb5d
jonasdegroote1 <degrjona@student.arteveldehs.be>
2024-05-28 14:57:57 +0200

    Update: category form

--- web/modules/custom/inventory_system/src/Form/CategoryForm.php
+++ web/modules/custom/inventory_system/src/Form/CategoryForm.php
@@ -4,43 +4,43 @@ namespace Drupal\inventory_system\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\taxonomy\Entity\Term;
+use Drupal\node\Entity\Node;
 
 class CategoryForm extends FormBase {
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId() {
-    return 'category_form';
+    return 'inventory_system_category_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state) {
-    $form['category_name'] = [
+    $form['name'] = [
       '#type' => 'textfield',
       '#title' => $this->t('Category Name'),
       '#required' => TRUE,
     ];
-    $form['description'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Description'),
-      '#required' => TRUE,
-    ];
+
     $form['submit'] = [
       '#type' => 'submit',
-      '#value' => $this->t('Add Category'),
+      '#value' => $this->t('Save'),
     ];
+
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $category_name = $form_state->getValue('category_name');
-    $description = $form_state->getValue('description');
-
-    // Save the category as a taxonomy term
-    $term = Term::create([
-      'name' => $category_name,
-      'vid' => 'category',
-      'description' => $description,
+    $node = Node::create([
+      'type' => 'category',
+      'title' => $form_state->getValue('name'),
     ]);
-    $term->save();
-
-    \Drupal::messenger()->addMessage($this->t('The category has been added.'));
+    $node->save();
+    \Drupal::messenger()->addMessage($this->t('Category saved.'));
   }
 }
