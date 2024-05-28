db21d23575ec6bd39bda4c9a553cd1d1b39d659c
jonasdegroote1 <degrjona@student.arteveldehs.be>
2024-05-28 15:40:15 +0200

    Add: delete functionality of category

--- web/modules/custom/inventory_system/src/Controller/CategoryController.php
+++ web/modules/custom/inventory_system/src/Controller/CategoryController.php
@@ -3,24 +3,61 @@
 namespace Drupal\inventory_system\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\Core\Link;
+use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\taxonomy\Entity\Term;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CategoryController extends ControllerBase {
+
+  /**
+   * The messenger service.
+   *
+   * @var \Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
+
+  /**
+   * Constructs a new CategoryController object.
+   *
+   * @param \Drupal\Core\Messenger\MessengerInterface $messenger
+   *   The messenger service.
+   */
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('messenger')
+    );
+  }
+
+  /**
+   * List all categories.
+   */
   public function listItems() {
-    $query = \Drupal::entityQuery('taxonomy_term');
-    $query->condition('vid', 'category');
-    $query->accessCheck(FALSE); // Set access check to FALSE
+    $query = \Drupal::entityQuery('taxonomy_term')
+      ->condition('vid', 'category')
+      ->accessCheck(FALSE); // Set access check to FALSE
     $tids = $query->execute();
 
     $terms = Term::loadMultiple($tids);
 
     $rows = [];
     foreach ($terms as $term) {
+      $deleteUrl = Url::fromRoute('inventory_system.category_delete', ['tid' => $term->id()]);
+      $deleteLink = Link::fromTextAndUrl($this->t('Delete'), $deleteUrl);
+
       $rows[] = [
         'data' => [
           $term->getName(),
           $term->getDescription(),
-          // Add more fields if needed
+          $deleteLink->toString(),
         ],
       ];
     }
@@ -28,7 +65,7 @@ class CategoryController extends ControllerBase {
     $header = [
       $this->t('Category Name'),
       $this->t('Description'),
-      // Add more headers if needed
+      $this->t('Actions'),
     ];
 
     $build = [
@@ -39,5 +76,27 @@ class CategoryController extends ControllerBase {
     ];
 
     return $build;
+  }
+
+  /**
+   * Delete a category.
+   *
+   * @param int $tid
+   *   The term ID of the category to delete.
+   */
+  public function deleteCategory($tid) {
+    // Convert the $tid parameter to an integer if necessary
+    $tid = (int) $tid;
+
+    // Load the term entity by its ID
+    $term = Term::load($tid);
+    if ($term) {
+      $term_name = $term->getName();
+      $term->delete();
+      $this->messenger->addMessage($this->t('Category %category has been deleted.', ['%category' => $term_name]));
+    } else {
+      $this->messenger->addMessage($this->t('Unable to delete category. Category not found.'), 'error');
+    }
+    return $this->redirect('inventory_system.category_list');
   }
 }
