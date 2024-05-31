27e88c038ca44f09d796481d0d89804463c81bcb
jonasdegroote1 <degrjona@student.arteveldehs.be>
2024-05-31 10:58:09 +0200

    Update: category edit form

--- web/modules/custom/inventory_system/src/Form/CategoryEditForm.php
+++ web/modules/custom/inventory_system/src/Form/CategoryEditForm.php
@@ -4,50 +4,94 @@ namespace Drupal\inventory_system\Form;
 
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\taxonomy\Entity\Term;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Drupal\Core\Database\Connection;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 class CategoryEditForm extends FormBase {
-  protected $term;
 
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $database;
+
+  /**
+   * Constructs a new CategoryEditForm object.
+   *
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database connection.
+   */
+  public function __construct(Connection $database) {
+    $this->database = $database;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('database')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function getFormId() {
     return 'category_edit_form';
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function buildForm(array $form, FormStateInterface $form_state, $tid = NULL) {
-    // Load the term entity.
-    $this->term = Term::load($tid);
-    if (!$this->term) {
-      throw new NotFoundHttpException();
+    if ($tid) {
+      // Fetch category data from the database.
+      $category = $this->database->select('categories', 'c')
+        ->fields('c', ['category_id', 'title'])
+        ->condition('category_id', $tid)
+        ->execute()
+        ->fetchAssoc();
+
+      // Build the form with category data.
+      if ($category) {
+        $form['category_id'] = [
+          '#type' => 'hidden',
+          '#value' => $category['category_id'],
+        ];
+        $form['category_name'] = [
+          '#type' => 'textfield',
+          '#title' => $this->t('Category Name'),
+          '#required' => TRUE,
+          '#default_value' => $category['title'],
+        ];
+        $form['submit'] = [
+          '#type' => 'submit',
+          '#value' => $this->t('Update Category'),
+        ];
+      }
     }
 
-    $form['category_name'] = [
-      '#type' => 'textfield',
-      '#title' => $this->t('Category Name'),
-      '#default_value' => $this->term->getName(),
-      '#required' => TRUE,
-    ];
-    $form['description'] = [
-      '#type' => 'textarea',
-      '#title' => $this->t('Description'),
-      '#default_value' => $this->term->getDescription(),
-      '#required' => TRUE,
-    ];
-    $form['submit'] = [
-      '#type' => 'submit',
-      '#value' => $this->t('Save Changes'),
-    ];
     return $form;
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $this->term->setName($form_state->getValue('category_name'));
-    $this->term->setDescription($form_state->getValue('description'));
-    $this->term->save();
+    $category_id = $form_state->getValue('category_id');
+    $category_name = $form_state->getValue('category_name');
+
+    // Update the category in the custom database table.
+    $this->database->update('categories')
+      ->fields([
+        'title' => $category_name,
+      ])
+      ->condition('category_id', $category_id)
+      ->execute();
 
     \Drupal::messenger()->addMessage($this->t('The category has been updated.'));
-
-    // Redirect to the category list page after saving changes.
     $form_state->setRedirect('inventory_system.category_list');
   }
 }
