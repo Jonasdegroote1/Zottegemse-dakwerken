b2ba5deeee0b6b71061f89a8bedb5967b3660bd1
jonasdegroote1 <degrjona@student.arteveldehs.be>
2024-05-31 15:09:47 +0200

    Update: available quantity

--- web/modules/custom/inventory_system/src/Form/AddToVehicleForm.php
+++ web/modules/custom/inventory_system/src/Form/AddToVehicleForm.php
@@ -72,7 +72,7 @@ class AddToVehicleForm extends FormBase {
       '#type' => 'table',
       '#header' => [
         $this->t('Item'),
-        $this->t('Available Stock'),
+        $this->t('available_quantity'),
         $this->t('Quantity'),
       ],
       '#empty' => $this->t('No inventory items available.'),
@@ -81,14 +81,15 @@ class AddToVehicleForm extends FormBase {
     // Get inventory items and populate the table.
     $inventory_items = $this->getInventoryItems();
     foreach ($inventory_items as $item) {
-      $available_quantity = $this->getAvailableQuantity($item->item_id);
-
-      $form['items'][$item->item_id]['title'] = [
-        '#markup' => $item->title,
+      $form['items'][$item->item_id]['checkbox'] = [
+        '#type' => 'checkbox',
+        '#title' => $item->title,
+        '#default_value' => FALSE,
       ];
 
-      $form['items'][$item->item_id]['available_stock'] = [
-        '#markup' => $available_quantity,
+      $form['items'][$item->item_id]['available_quantity'] = [
+        '#type' => 'item',
+        '#markup' => $this->getAvailableQuantity($item->item_id),
       ];
 
       $form['items'][$item->item_id]['quantity'] = [
@@ -107,7 +108,6 @@ class AddToVehicleForm extends FormBase {
 
     return $form;
   }
-
 
   /**
    * {@inheritdoc}
@@ -130,8 +130,8 @@ class AddToVehicleForm extends FormBase {
   }
 
   /**
-  * {@inheritdoc}
-  */
+   * {@inheritdoc}
+   */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $vehicle_id = $form_state->getValue('vehicle');
 
@@ -145,10 +145,6 @@ class AddToVehicleForm extends FormBase {
       $selected_quantity = $form_state->getValue($quantity_field);
       $available_quantity = $this->getAvailableQuantity($item_id);
 
-      // Check if the item is associated with another vehicle and subtract its quantity.
-      $previous_quantity = $this->getVehicleItemQuantity($vehicle_id, $item_id);
-      $available_quantity -= $previous_quantity;
-
       // Check if selected quantity exceeds available quantity.
       if ($selected_quantity > 0 && $selected_quantity <= $available_quantity) {
         // Insert the data into the item_vehicle table.
@@ -160,33 +156,15 @@ class AddToVehicleForm extends FormBase {
           ])
           ->execute();
 
-        // Subtract the added quantity from the item's quantity.
-        $updated_quantity = $available_quantity - $selected_quantity;
-        $this->updateItemQuantity($item_id, $updated_quantity);
-
         $added_items++;
-      } elseif ($selected_quantity > 0) {
-        // Display error message if selected quantity exceeds available quantity.
-        $this->messenger()->addError($this->t('Selected quantity exceeds available quantity for %item.', ['%item' => $item->title]));
       }
     }
 
     // Display success message if items were added.
     if ($added_items > 0) {
-      $this->messenger()->addStatus($this->formatPlural($added_items, 'One item added to the vehicle.', '@count items added to the vehicle.'));
+      $this->messenger()->addStatus($this->formatPlural($added_items, 'One item added to the vehicle.', '@count items added to the' . ' vehicle.'));
     }
   }
-
-/**
- * Helper function to update the quantity of an item.
- */
-public function updateItemQuantity($item_id, $quantity) {
-  $this->database->update('items')
-    ->fields(['quantity' => $quantity])
-    ->condition('item_id', $item_id)
-    ->execute();
-}
-
   /**
    * Helper function to retrieve inventory items.
    */
@@ -227,16 +205,5 @@ public function updateItemQuantity($item_id, $quantity) {
     }
 
     return $vehicles;
-  }
-
-  public function getVehicleItemQuantity($vehicle_id, $item_id) {
-    $query = $this->database->select('item_vehicle', 'iv')
-      ->fields('iv', ['quantity'])
-      ->condition('vehicle_id', $vehicle_id)
-      ->condition('item_id', $item_id)
-      ->execute()
-      ->fetchField();
-
-    return $query ?: 0;
   }
 }
